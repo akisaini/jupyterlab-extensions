28c7c74c76e08b9dc9b758afe0b133ab52c3b3a5
Akshat Saini <akshatsaini1@gmail.com>
2024-07-01 15:45:08 -0400

    Update metadata.py

--- tornado_serving/array_extract.py
+++ tornado_serving/array_extract.py
@@ -1,9 +1,11 @@
 import numpy as np
 import tifffile
-import matplotlib.pyplot as plt
 
 def load_image(file_path):
     with tifffile.TiffFile(file_path) as tif:
         image_data = tif.asarray() # Reads the tiff data into a numpy array
     return image_data
 
+ome_path = 'custom_ome_tiff.ome.tif'
+numpy_ome_array = load_image(ome_path)
+print(numpy_ome_array)
--- tornado_serving/metadata.py
+++ tornado_serving/metadata.py
@@ -2,19 +2,18 @@ import tifffile
 
 ome_tiff_path = '/Users/sainia2/Documents/pj/forks/jupyterlab-extensions/tornado_serving/custom_ome_tiff.ome.tif'
 
-# Open the OME-TIFF file
+# Open file
 with tifffile.TiffFile(ome_tiff_path) as tif:
-    # Print the OME-XML metadata
     ome_metadata = tif.ome_metadata
     print("OME-XML Metadata:\n", ome_metadata)
 
-    # Print the IFD entries for each image in the TIFF file
+    # Print IFD entries for each image
     for page in tif.pages:
         print(f"Page {page.index}:")
         for tag in page.tags.values():
             print(f"  {tag.name}: {tag.value}")
 
-    # Print additional details from the OME-TIFF file
+    # Print additional details
     print("\nDetailed IFD entries:")
     for page in tif.pages:
         for tag in page.tags.values():
